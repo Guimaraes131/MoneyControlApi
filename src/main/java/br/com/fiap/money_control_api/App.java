b29ea65d80beec5092760af3850a29eb9a86aa7c
Guilherme <guilhermegpereira26@hotmail.com>
2025-02-24 10:21:13 -0300

    adicionando GET e classe de Categoria

--- src/main/java/br/com/fiap/money_control_api/App.java
+++ src/main/java/br/com/fiap/money_control_api/App.java
@@ -3,16 +3,13 @@ package br.com.fiap.money_control_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-	}
-
-	@RequestMapping(path = "/")
-	public String index() {
-		return "ok";
 	}
 }
---
+++ src/main/java/br/com/fiap/money_control_api/controller/CategoryController.java
@@ -0,0 +1,15 @@
+package br.com.fiap.money_control_api.controller;
+
+import br.com.fiap.money_control_api.model.Category;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController {
+
+    @RequestMapping(path = "/category", produces = "application/json", method = {RequestMethod.GET})
+    public Category index() {
+        return new Category(1L, "Educação", "book");
+    }
+}
